7e380214f591f6af23fff28a9738eaf92753772d
William Malone <w@wmalone.com>
2014-07-01 08:41:04 -0600

    Build 0.3.13

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,16 +4,88 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 	concat: {
-    // concat task configuration goes here.
+		index: {
+            options: {
+                process: function(src, filepath) {
+                
+                	var file = src,
+                		version = grunt.file.readJSON('package.json').version;
+                	
+                	file = file.replace(/<!--\WBlocksJS\WFramework\WCSS\WSTART\W-->([\s\S]*?)END\W-->/, '<link rel="stylesheet" href="css/blocksjs-' + version + '.min.css">');
+                    //file = file.replace(/<!--\W\BlocksJS\WFramework\WJavaScript\W-\START\W-->\([\s\S]*?)END\W-->/, '<script src="thirdParty/blocksjs/js/blocksjs-<%= pkg.version %>.min.css"></script>');
+                    
+                    file = file.replace(/<!--\WBlocksJS\WFramework\WJS\WSTART\W-->([\s\S]*?)END\W-->/, '<script src="js/blocksjs-' + version + '.min.js"></script>');
+                    //file = file.replace(/<!--\W[BLOCKSJS\WENGINE\W-\WSTART\]\W-->([\s\S]*?)END\]\W-->/, '<script src="thirdParty/blocksjs/js/blocksjs.min.js"></script>');
+                    
+                    return file;
+                }
+            },
+            files: [{
+					src: 'src/index.html',
+                    dest: 'build/<%= pkg.version %>/index.html'
+				}
+			]
+        }
 	},
+	copy: {
+        /*archive: {
+            files: [
+                {
+                    expand: true, 
+                    cwd: '',
+                    src: '**',
+                    dest: 'archive/<%= pkg.version %>/', 
+                    filter: 'isFile'
+                }
+            ]
+        },*/
+        build: {
+            files: [
+                {
+                    expand: true, 
+                    cwd: 'images',
+                    src: '**',
+                    dest: 'build/<%= pkg.version %>/images/', 
+                    filter: 'isFile'
+                },
+                {
+                    expand: true,
+                    cwd: 'css',
+                    src: '**',
+                    dest: 'build/<%= pkg.version %>/css/', 
+                    filter: 'isFile'
+                },
+                {
+                    expand: true, 
+                    cwd: 'fonts',
+                    src: '**',
+                    dest: 'build/<%= pkg.version %>/fonts/', 
+                    filter: 'isFile'
+                },
+                {
+                    expand: true, 
+                    cwd: 'audio',
+                    src: '**',
+                    dest: 'build/<%= pkg.version %>/audio/', 
+                    filter: 'isFile'
+                }
+            ]
+        }
+    },
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= pkg.version %> <%= grunt.template.today("yyyy-mm-dd") %>\n\nCopyright (c) 2014 William Malone (www.williammalone.com)\n\n' + 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\n' + 'The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\n' + 'THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.\n*/\n\n'
       },
       build: {
         src: 'src/**/*.js',
-        dest: 'build/js/blocksjs-<%= pkg.version %>.min.js'
+        dest: 'build/<%= pkg.version %>/js/blocksjs-<%= pkg.version %>.min.js'
       }
+    },
+    cssmin: {
+        build: {
+            src: 'src/**/*.css',
+            dest: 'build/<%= pkg.version %>/css/blocksjs-<%= pkg.version %>.min.css'
+        }
     },
 	jshint: {
 		src: 'src/blocks/**/*.js',
@@ -85,9 +157,14 @@ module.exports = function(grunt) {
 	}
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
-  grunt.registerTask('default', ['jshint', 'uglify']);
+  // Load the plugins
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-contrib-compress');
+	
+	// Default task(s).
+	grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy']);
 
 };
